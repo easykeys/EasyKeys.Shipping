9d6b938bfe89ec1c350899d96f8acc2ba5f5f0b7
kdcllc <info@kingdavidconsulting.com>
2021-10-22 17:54:25 -0400

    adding stamps.com project

---
+++ src/EasyKeys.Shipping.Stamps.Abstractions/Options/StampsOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyKeys.Shipping.Stamps.Abstractions.Options
+{
+    public class StampsOptions
+    {
+        public bool IsProduction{ get; set; }
+
+        public string Url => IsProduction ? "https://swsim.stamps.com/swsim/swsimv111.asmx" : "https://swsim.testing.stamps.com/swsim/swsimv111.asmx";
+
+        public string IntegrationId { get; set; } = string.Empty;
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ test/EasyKeys.Shipping.UnitTest/StampsRateProviderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bet.Extensions.Testing.Logging;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Xunit.Abstractions;
+
+namespace EasyKeysShipping.UnitTest
+{
+    public class StampsRateProviderTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public StampsRateProviderTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private ServiceProvider GetServices()
+        {
+            var services = new ServiceCollection();
+            var dic = new Dictionary<string, string>
+            {
+                { "AzureVault:BaseUrl", "https://easykeys.vault.azure.net/" },
+            };
+
+            var configBuilder = new ConfigurationBuilder().AddInMemoryCollection(dic);
+
+            configBuilder.AddAzureKeyVault(hostingEnviromentName: "Development", usePrefix: true);
+
+            services.AddLogging(builder => builder.AddXunit(_output));
+            services.AddSingleton<IConfiguration>(configBuilder.Build());
+            services.AddFedExRateProvider();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
