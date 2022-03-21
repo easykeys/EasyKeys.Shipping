3d58d2f427107f9b15c571cebfdba19e655f799d
brandon <bmoffett@easykeys.com>
2022-03-21 13:23:07 -0700

    small clean up

--- src/EasyKeys.Shipping.FedEx.Shipment/Models/LabelOptions.cs
+++ src/EasyKeys.Shipping.FedEx.Shipment/Models/LabelOptions.cs
@@ -28,7 +28,6 @@ namespace EasyKeys.Shipping.FedEx.Shipment
 
         public CodDetail CodDetail { get; set; } = new CodDetail();
 
-
         public PaymentType PaymentType { get; set; }
 
         /// <summary>
@@ -36,7 +35,6 @@ namespace EasyKeys.Shipping.FedEx.Shipment
         /// be populated. Required fields are either the company/person name and the
         /// phone number. Required address fields are the street line 1, city, state/provice
         /// code and country code.
-        ///
         /// </summary>
         public Party ResponsibleParty { get; set; } = new Party();
 
@@ -46,20 +44,16 @@ namespace EasyKeys.Shipping.FedEx.Shipment
         public string AccountNumber { get; set; } = string.Empty;
 
         /// <summary>
-        /// Required to receive the correct label image in the Ship Reply service:
+        /// Required to receive the correct label image in the Ship Reply service.
         /// Valid values:
         /// • COMMON2D — label format type to receive a label.
-        /// • LABEL_DATA_ONLY — this value is used to receive
-        /// the barcode data if you create a custom label.
+        /// • LABEL_DATA_ONLY — this value is used to receive the barcode data if you create a custom label.
         /// </summary>
         public LabelFormatType LabelFormatType { get; set; }
 
         /// <summary>
         /// Required to indicate label formatting. Type of data stream or bitmap to be returned:
         /// Valid values are:
-        /// Create a Label
-        /// FedEx Web Services, Developer Guide 2020 134
-        /// Web Services Element Description
         /// • PDF — plain paper
         /// • PNG — plain paper
         /// • DOC
