a0b095c3c35b72945a22928a8be7d371bab260e0
brandon <bmoffett@easykeys.com>
2022-05-04 18:15:40 -0700

    added stamps rate configurator

---
+++ src/EasyKeys.Shipping.Stamps.Rates/Extensions/PackageExtensions.cs
@@ -0,0 +1,80 @@
+﻿using EasyKeys.Shipping.Abstractions.Models;
+
+namespace EasyKeys.Shipping.Stamps.Rates.Extensions
+{
+    public static class PackageExtensions
+    {
+        public static bool IsLargeFlatEnvelope(this Package package)
+        {
+            package = package ?? throw new ArgumentNullException(nameof(package));
+
+            return package.Weight < 0.8125m
+                && package.Dimensions.Length <= 15m
+                && package.Dimensions.Width <= 12m;
+        }
+
+        /// <summary>
+        /// 12 1/2″ x 9 1/2″
+        /// </summary>
+        /// <param name="pacakge"></param>
+        /// <returns></returns>
+        public static bool IsPaddedFlatRateEnvelope(this Package package)
+        {
+            package = package ?? throw new ArgumentNullException(nameof(package));
+
+            return package.Weight < 0.8125m &&
+                package.Dimensions.Length <= 12.5m &&
+                package.Dimensions.Width <= 9.5m &&
+                package.Dimensions.Height <= 4m;
+        }
+
+        /// <summary>
+        /// Small Flat Rate Box	USPS small flat rate box. A special 8-5/8” x 5-3/8” x 1-5/8” USPS box that clearly indicates “Small Flat Rate Box”.
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool IsSmallFlatRateBox(this Package package)
+        {
+            package = package ?? throw new ArgumentNullException(nameof(package));
+
+            return package.Dimensions.Length <= 8.625m &&
+                package.Dimensions.Width <= 5.375m &&
+                package.Dimensions.Height <= 1.625m;
+        }
+
+        /// <summary>
+        /// Flat Rate Box	USPS medium flat rate box. A special 11” x 8 ½” x 5 ½” or 14” x 3.5” x 12” USPS box that clearly indicates “Medium Flat Rate Box”
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool IsFlatRateBox(this Package package)
+        {
+            package = package ?? throw new ArgumentNullException(nameof(package));
+
+            return (package.Dimensions.Length <= 11m &&
+                package.Dimensions.Width <= 8.5m &&
+                package.Dimensions.Height <= 5.55m)
+                ||
+                (package.Dimensions.Length <= 14m &&
+                package.Dimensions.Width <= 3.5m &&
+                package.Dimensions.Height <= 12m);
+        }
+
+        /// <summary>
+        /// Large Flat Rate Box	USPS large flat rate box. A special 12” x 12” x 6” USPS box that clearly indicates “Large Flat Rate Box”.
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool IsLargeFlatRateBox(this Package package)
+        {
+            package = package ?? throw new ArgumentNullException(nameof(package));
+
+            return package.Dimensions.Length <= 12m &&
+                package.Dimensions.Width <= 12m &&
+                package.Dimensions.Height <= 6m;
+        }
+    }
+}
---
+++ src/EasyKeys.Shipping.Stamps.Rates/StampsRateConfigurator.cs
@@ -0,0 +1,167 @@
+﻿
+using EasyKeys.Shipping.Abstractions.Models;
+using EasyKeys.Shipping.Stamps.Abstractions.Models;
+using EasyKeys.Shipping.Stamps.Rates.Extensions;
+
+namespace EasyKeys.Shipping.Stamps.Rates
+{
+    public class StampsRateConfigurator
+    {
+        public StampsRateConfigurator(
+            Address origin,
+            Address destination,
+            Package package,
+            DateTime? shipDate = null)
+        {
+            var solidDate = shipDate ?? DateTime.Now.AddDays(1);
+
+            if (destination.IsUnitedStatesAddress())
+            {
+                // 1 lb = 16 oz => goes to first class
+                if (package.Weight <= 0.999375m)
+                {
+                    ConfigureDomesticFirstClass(origin, destination, package, solidDate);
+                }
+
+                // 1 < weight < 70 => goes to priority shipping
+                else
+                {
+                    ConfigureDomesticPriority(origin, destination, package, solidDate);
+                }
+            }
+            else
+            {
+                // international first class
+                // international priority
+            }
+        }
+
+        public List<(Shipment shipment, RateRequestDetails rateOptions)> Shipments { get; } = new List<(Shipment shipment, RateRequestDetails rateOptions)>();
+
+        /// <summary>
+        /// just holders for now
+        /// </summary>
+        /// <param name="weight"></param>
+        /// <returns></returns>
+        public static Package GetLargeFlatEnvelope(decimal weight)
+        {
+            return new Package(14.9m, 11.9m, 4m, weight, 0m);
+        }
+
+        public static Package GetFirstClassPackage(decimal weight)
+        {
+            return new Package(11m, 5m, 4m, weight, 0m);
+        }
+
+        public static Package GetSmallFlaxBox(decimal weight)
+        {
+            return new Package(11m, 5m, 4m, weight, 0m);
+        }
+
+        public static Package GetMediumFlatBox(decimal weight)
+        {
+            return new Package(11m, 5m, 4m, weight, 0m);
+        }
+
+        public static Package GetIntlEnvelope(decimal weight)
+        {
+            return new Package(11m, 5m, 4m, weight, 0m);
+        }
+
+        public static Package GetIntlBox(decimal weight)
+        {
+            return new Package(11m, 5m, 4m, weight, 0m);
+        }
+
+        private void ConfigureDomesticFirstClass(
+        Address origin,
+        Address destination,
+        Package package,
+        DateTime shipDate)
+        {
+            // 1 lb = 16 oz, must be less than 15.99
+            if (package.Weight > 0.999375m)
+            {
+                throw new ArgumentException("First Class Mail package can't be greater than 15.99 oz", nameof(package.Weight));
+            }
+
+            var packages = new List<Package> { package };
+            var options = new RateRequestDetails()
+            {
+                PackageType = package.IsLargeFlatEnvelope() ? PackageType.Large_Envelope_Or_Flat : PackageType.Package,
+                ServiceDescription = "USPS First Class Mail",
+                ServiceType = ServiceType.USPS_FIRST_CLASS_MAIL
+            };
+            var shipmentOptions = new ShipmentOptions()
+            {
+                ShippingDate = shipDate
+            };
+
+            var shipment = new Shipment(origin, destination, packages, shipmentOptions);
+
+            Shipments.Add((shipment, options));
+        }
+
+        private void ConfigureDomesticPriority(
+        Address origin,
+        Address destination,
+        Package package,
+        DateTime shipDate)
+        {
+            if (package.Weight > 70)
+            {
+                throw new ArgumentException("Priority Mail package can't be greater than 70 lbs", nameof(package.Weight));
+            }
+
+            var packages = new List<Package> { package };
+
+            var shipOptions = new ShipmentOptions
+            {
+                ShippingDate = shipDate
+            };
+
+            var rateOptions = new RateRequestDetails()
+            {
+                ServiceDescription = "USPS Priority Mail",
+                ServiceType = ServiceType.USPS_PRIORITY_MAIL,
+                PackageType = GetFlatRatePackage(package)
+            };
+        }
+
+        private PackageType GetFlatRatePackage(Package package)
+        {
+            // sequence from smallest to largest
+            if (package.IsPaddedFlatRateEnvelope())
+            {
+                return PackageType.Flat_Rate_Padded_Envelope;
+            }
+
+            if (package.IsSmallFlatRateBox())
+            {
+                return PackageType.Small_Flat_Rate_Box;
+            }
+
+            if (package.IsFlatRateBox())
+            {
+                return PackageType.Flat_Rate_Box;
+            }
+
+            if (package.IsLargeFlatRateBox())
+            {
+                return PackageType.Large_Flat_Rate_Box;
+            }
+
+            return PackageType.Package;
+        }
+
+        private object ConfigureLargePackage() => null;
+
+        private object ConfigureDomesticSmallFlatRate() => null;
+
+        private object ConfigureDomesticMediumFlatRate() => null;
+
+        private object ConfigureIntlPriorityMail() => null;
+
+        private object ConfigureIntlFirstClass() => null;
+    }
+}
